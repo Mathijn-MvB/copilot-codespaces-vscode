bd1620d7fae7bca903e2653ebf54ed616b1ad5a0
Mathijn-MvB <127858302+Mathijn-MvB@users.noreply.github.com>
2023-08-05 15:14:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var skills = {
+        name: 'John Smith',
+        age: 35,
+        skills: ['JavaScript', 'React', 'Node', 'MongoDB']
+    };
+    console.log(skills.name);
+    console.log(skills.age);
+    console.log(skills.skills);
+    console.log(skills.skills[2]);
+}
